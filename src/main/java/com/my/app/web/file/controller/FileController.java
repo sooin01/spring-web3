3ec19ba4ad5957eaafa7e906c85b2268347c67cd
sooin <sooin@sooin-PC>
2016-11-30 00:51:17 +0900

    add

--- src/main/java/com/my/app/web/file/controller/FileController.java
+++ src/main/java/com/my/app/web/file/controller/FileController.java
@@ -9,7 +9,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.ProgressListener;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -37,7 +44,7 @@ public class FileController {
 		return "file/upload";
 	}
 	
-	@PostMapping(value = "/upload")
+//	@PostMapping(value = "/upload")
 	public ResponseEntity<Void> upload(ServletInputStream sis) {
 		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss.SSS");
 		
@@ -60,76 +67,67 @@ public class FileController {
 //	@PostMapping(value = "/upload")
 	public ResponseEntity<Void> upload(FileDto fileDto) {
 //		fileService.file(fileDto);
-		
-		Path path = Paths.get("C:/Users/sooin/Desktop/test.avi");
-		path.toFile().delete();
-		
-		try (BufferedInputStream bis = new BufferedInputStream(fileDto.getFile().getInputStream());
-				FileOutputStream fos = new FileOutputStream(path.toFile())) {
-			byte[] b = new byte[4096];
-			
-			int read = -1;
-			while ((read = bis.read(b)) != -1) {
-				fos.write(b, 0, read);
-				log.debug("Read: {}", read);
-			}
-		} catch (Exception e) {
-			log.error("File exception.", e);
-		}
-		
 		return ResponseEntity.ok().build();
 	}
 	
-//	@PostMapping(value = "/upload")
-//	public ResponseEntity<Void> upload(HttpServletRequest request) throws Exception {
-//		boolean multipart = ServletFileUpload.isMultipartContent(request);
-//		log.debug("multipart: {}", multipart);
-//		
-//		FileDto fileDto = new FileDto();
-//		
+	@PostMapping(value = "/upload")
+	public ResponseEntity<Void> upload(HttpServletRequest request) throws Exception {
+		boolean multipart = ServletFileUpload.isMultipartContent(request);
+		log.debug("multipart: {}", multipart);
+		
+		FileDto fileDto = new FileDto();
+		
 //		BindingResult bindingResult = new BeanPropertyBindingResult(fileDto, "fileDto");
 //		validator.validate(fileDto, bindingResult);
-//		
+		
 //		DataBinder binder = new DataBinder(fileDto);
 //		binder.setValidator(validator);
 //		binder.validate();
 //		BindingResult bindingResult = binder.getBindingResult();
-//		
+		
 //		if (bindingResult.hasErrors()) {
 //			FieldError fieldError = bindingResult.getFieldError();
 //			log.error("{}, {}", fieldError.getField(), fieldError.getDefaultMessage());
 //		}
-//		
-//		ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
-//		FileItemIterator iter = upload.getItemIterator(request);
-//		while (iter.hasNext()) {
-//			FileItemStream fileItem = iter.next();
-//			
-//			if (fileItem.isFormField()) {
-//				log.debug("name: {}, value: {}", fileItem.getFieldName(), Streams.asString(fileItem.openStream()));
-//			} else {
-//				log.debug("name: {}, name: {}", fileItem.getFieldName(), fileItem.getName());
-//				
-//				Path path = Paths.get("C:/Users/sooin/Desktop/" + fileItem.getName());
-//				
-//				try (BufferedInputStream bis = new BufferedInputStream(fileItem.openStream());
-//						FileOutputStream fos = new FileOutputStream(path.toFile())) {
-//					byte[] b = new byte[100 * 1024];
-//					int read = -1;
-//					while ((read = bis.read(b)) != -1) {
-//						fos.write(b, 0, read);
-//						fos.flush();
-//					}
-//				} catch (Exception e) {
-//					log.error("file upload exception!", e);
-//				}
-//				
-//				log.debug("file size: {}", path.toFile().length());
-//				path.toFile().delete();
-//			}
-//		}
-//		
-//		return ResponseEntity.ok().build();
-//	}
+		
+		ProgressListener progressListener = new ProgressListener() {
+			@Override
+			public void update(long pBytesRead, long pContentLength, int pItems) {
+				log.debug(">>> {}, {}, {}", pBytesRead, pContentLength, pItems);
+			}
+		};
+		
+		ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
+		upload.setProgressListener(progressListener);
+		FileItemIterator iter = upload.getItemIterator(request);
+		while (iter.hasNext()) {
+			FileItemStream fileItem = iter.next();
+			
+			if (fileItem.isFormField()) {
+				log.debug("name: {}, value: {}", fileItem.getFieldName(), Streams.asString(fileItem.openStream()));
+			} else {
+				log.debug("name: {}, name: {}", fileItem.getFieldName(), fileItem.getName());
+				
+				Path path = Paths.get("C:/Users/sooin/Desktop/" + fileItem.getName());
+				
+				try (BufferedInputStream bis = new BufferedInputStream(fileItem.openStream());
+						FileOutputStream fos = new FileOutputStream(path.toFile())) {
+					byte[] b = new byte[100 * 1024];
+					int read = -1;
+					while ((read = bis.read(b)) != -1) {
+						fos.write(b, 0, read);
+						fos.flush();
+					}
+				} catch (Exception e) {
+					log.error("file upload exception!", e);
+				}
+				
+				log.debug("file size: {}", path.toFile().length());
+				path.toFile().delete();
+			}
+		}
+		
+		return ResponseEntity.ok().build();
+	}
 	
 }
