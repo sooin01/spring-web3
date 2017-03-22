10ebaae2e5683574a4c388880bcb29337749508f
sooin <sooin@sooin01-PC>
2017-03-22 20:55:55 +0900

    add

--- src/main/java/com/my/app/api/controller/ApiController.java
+++ src/main/java/com/my/app/api/controller/ApiController.java
@@ -27,6 +27,9 @@ public class ApiController {
 		map.put("id", "test");
 		map.put("name", "test");
 		map.put("age", 20);
+		map.put("age", 20);
+		map.put("age", 20);
+		map.put("age", 20);
 
 		return map;
 	}
